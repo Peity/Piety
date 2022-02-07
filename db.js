1203534ae40462107e24b86d352ab4548c39b6ae
Mahyar Behzadi <behzadimahyar85@gmail.com>
2022-02-07 09:36:02 +0330

    Create .env

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
+require('dotenv').config()
 
 var mysql = require('mysql')
 var connection = mysql.createConnection({
-    host: 'localhost',
-    port: '3306',
-    user: 'root',
-    password: '',
-    database: 'piety'
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 })
 
 
