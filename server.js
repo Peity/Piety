ddc328e949fa0109459afbc1632d946a20d81be2
Mr.Sha1 <38698531+mrshaone@users.noreply.github.com>
2022-02-08 02:09:13 +0330

    Prisma added for ORM

--- server.js
+++ server.js
@@ -12,15 +12,6 @@ app.get('/', (req, res) => {
 app.listen(port)
 
 
-const db = require('./db')
-const connection = db.connection
-
-connection.connect()
-
-connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields){
-    if (err) throw err
-
-    console.log('The solution is: ', rows[0].solution)
-
-})
+const { PrismaClient } =  require('@prisma/client')
+const prisma = new PrismaClient()
 
