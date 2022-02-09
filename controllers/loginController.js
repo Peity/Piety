1929332161ac5af93a669809cb9928b56f6b6e0e
Mr.Sha1 <38698531+mrshaone@users.noreply.github.com>
2022-02-09 23:41:47 +0330

    FKING ASYNC :|

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -11,7 +11,19 @@ const loginView = (req, res) => {
     res.render("login", {
     } )
 }
+
+const getPlayer = (req, res) => {
+    const username = req.params.username
+    const {findPlayer} = require('../models/Player.js')
+    findPlayer(username).then(
+        function(result){
+            res.send(result)
+        }
+    )
+    
+}
 module.exports =  {
     registerView,
-    loginView
+    loginView,
+    getPlayer
 };
--- routers/login.js
+++ routers/login.js
@@ -1,7 +1,8 @@
 //js
 const express = require('express')
-const {registerView, loginView } = require('../controllers/loginController')
+const {registerView, loginView, getPlayer } = require('../controllers/loginController')
 const router = express.Router()
 router.get('/register', registerView)
 router.get('/login', loginView)
+router.get('/player/:username', getPlayer)
 module.exports = router
