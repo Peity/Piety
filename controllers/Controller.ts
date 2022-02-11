e5ea3c4a6d3aa654faba0d95969472ef48f48e4f
Mr.Sha1 <38698531+mrshaone@users.noreply.github.com>
2022-02-11 23:31:31 +0330

    FU

--- controllers/Controller.ts
+++ controllers/Controller.ts
@@ -2,6 +2,6 @@ import express from 'express';
 
 export default interface controller{
     index(): JSON;
-    show(id: any, res: express.Response): express.Response;
+    show(id: any, res: express.Response): Promise<JSON>;
     create(req: express.Request, res: express.Response): express.Response;
 }
