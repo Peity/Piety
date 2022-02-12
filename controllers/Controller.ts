2ac2bca012f8b2054619314e83e13ea950f1fd35
Mr.Sha1 <38698531+mrshaone@users.noreply.github.com>
2022-02-12 12:56:23 +0330

    Controller interface updated

--- controllers/Controller.ts
+++ controllers/Controller.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 
 export default interface controller{
-    index(req: express.Request, res: express.Response): Promise<any>;
-    show(id: any, res: express.Response): any;
+    index(req: express.Request, res: express.Response): Promise<Response>;
+    show(id: any, res: express.Response): Promise<Response>;
     create(req: express.Request, res: express.Response): Promise<Response>;
+    update(id: any, req:express.Request, res: express.Response): Promise<Response>;
+    delete(id: any, res:express.Response): Promise<Response>;
 }
